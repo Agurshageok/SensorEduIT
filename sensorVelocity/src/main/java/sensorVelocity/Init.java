df37d17a6469e5ea8c46c7dd9c83fc7b05226b45
santiago.ferreiro <santiago.ferreiro@CPX-BLFU23PERJG.dir.svc.accenture.com>
2019-07-15 12:38:20 -0300

    tupla terminada2

--- sensorVelocity/src/main/java/sensorVelocity/Init.java
+++ sensorVelocity/src/main/java/sensorVelocity/Init.java
@@ -5,6 +5,24 @@ public class Init {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		sensorvelocidad.Sensor sV = new sensorvelocidad.Sensor();
+		sensorclima.Sensor sC
+		while(true) {
+			
+			
+			
+			
+			
+			
+			
+			
+			try {
+				Thread.sleep(1000*60);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
--- sensorVelocity/src/main/java/sensorVelocity/Tupla.java
+++ sensorVelocity/src/main/java/sensorVelocity/Tupla.java
@@ -1,5 +1,52 @@
 package sensorVelocity;
 
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 public class Tupla {
+	
+	private sensorvelocidad.TipoVehiculo tipoVehiculo;
+	private sensorclima.TipoClima tipoClima;
+	
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + ((tipoClima == null) ? 0 : tipoClima.hashCode());
+//		result = prime * result + ((tipoVehiculo == null) ? 0 : tipoVehiculo.hashCode());
+//		return result;
+//	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tupla other = (Tupla) obj;
+		if (tipoClima != other.tipoClima)
+			return false;
+		if (tipoVehiculo != other.tipoVehiculo)
+			return false;
+		return true;
+	}
+	public sensorvelocidad.TipoVehiculo getTipoVehiculo() {
+		return tipoVehiculo;
+	}
+	public void setTipoVehiculo(sensorvelocidad.TipoVehiculo tipoVehiculo) {
+		this.tipoVehiculo = tipoVehiculo;
+	}
+	public sensorclima.TipoClima getTipoClima() {
+		return tipoClima;
+	}
+	public void setTipoClima(sensorclima.TipoClima tipoClima) {
+		this.tipoClima = tipoClima;
+	}
+	
+	public int hashcode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+	
+	
 
 }
